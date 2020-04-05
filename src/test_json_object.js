c3002e285afd81be23ef1f894a4f814637798359
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-05 12:14:01 -0700

    Create demo json object. Begin testing

--- src/test_json_object.js
+++ src/test_json_object.js
@@ -1,134 +1,55 @@
 var testJasonObject = [
     {
-        'firstName': 'FC Tulsa',
-        'lastName': 'TBD',
-        'role': '2020-03-07',
-        'vacationHoursRemaining':'March',
-        'healthPlanName':'Mar',
-        'startDate':'Papa Murphy&apos;s Park',
-        'directManager':'',
-        'broadcast':'KQCA My58, Estrella TV Sacramento, ESPN+',
-        'time':'TBD',
-        'active': true,
-        'promoContent': {
-            'title':'Opening Night',
-            'description':'Exclusive SRFC flag giveaway – Post-match fireworks'
-        },
-        'ticketURL': 'https://www1.ticketmaster.com/sacramento-republic-fc-vs-fc-tulsa/event/1C00583DBAEF7791'
+        'firstName': 'Jim',
+        'lastName': 'Jones',
+        'fullName': 'Jim Jones',
+        'role': 'Office Admin',
+        'vacationHoursRemaining':'14',
+        'healthPlanName':'Health Plan A',
+        'startDate':'2020-01-25',
+        'directManager':'Linnea Jones'
     },
+    {
+      'firstName': 'Samantha',
+      'lastName': 'Jacobs',
+      'fullName': 'Samantha Jacobs',
+      'role': 'Account Executive',
+      'vacationHoursRemaining':'24',
+      'healthPlanName':'Health Plan C',
+      'startDate':'2019-09-25',
+      'directManager':'Linnea Jones'
+    },
+    {
+      'firstName': 'Bruce',
+      'lastName': 'Stevenson',
+      'fullName': 'Bruce Stevenson',
+      'role': 'Customer Advocate',
+      'vacationHoursRemaining':'17',
+      'healthPlanName':'Health Plan B',
+      'startDate':'2019-11-25',
+      'directManager':'Linnea Jones'
 
+    },
+    {
+      'firstName': 'Linnea',
+      'lastName': 'Jones',
+      'fullName': 'Linnea Jones',
+      'role': 'VP of Sales',
+      'vacationHoursRemaining':'30',
+      'healthPlanName':'Health Plan B',
+      'startDate':'2018-09-25',
+      'directManager':'Toni Braxton'
 
-    role
-    vacationHoursRemaining
-    healthPlanName
-    startDate
-    directManager
-    {
-        'team':'Reno 1868 FC',
-        'date':'TBD',
-        'isoDate': '2020-03-25',
-        'month':'March',
-        'monthShort':'Mar',
-        'location':'Papa Murphy&apos;s Park',
-        'presaleDescription':'',
-        'broadcast':'ESPN+',
-        'time':'TBD',
-        'active': true ,
-        'promoContent': {
-            'title':'White Claw Wednesday',
-            'description':'Cheer on Republic FC with White Claws for only $9'
-        },
-        'ticketURL': 'https://www.ticketmaster.com/sacramento-republic-fc-vs-reno-1868-fc/event/1C00583DBAF47793'
     },
     {
-        'team':'OKC Energy FC',
-        'date':'TBD',
-        'isoDate': '2020-03-29',
-        'month':'March',
-        'monthShort':'Mar',
-        'location':'Papa Murphy&apos;s Park',
-        'presaleDescription':'',
-        'broadcast':'KQCA My58, Estrella TV Sacramento, ESPN+',
-        'time':'TBD',
-        'active': true ,
-        'promoContent': {
-            'title':'Family Day',
-            'description':'4 Tickets for $49 or 4 Tickets, Hot dogs, Sodas for $89'
-        },
-        'ticketURL': 'https://www1.ticketmaster.com/sacramento-republic-fc-vs-okc-energy-fc/event/1C00583DBAF7779D'
-    },
-    {
-        'team':'Oakland Roots',
-        'date':'TBD',
-        'isoDate': '2020-04-09',
-        'month':'April',
-        'monthShort':'Apr',
-        'location':'Papa Murphy&apos;s Park',
-        'presaleDescription':'',
-        'broadcast':'',
-        'time':'TBD',
-        'active': true ,
-        'promoContent': {
-            'title':'U.S. Open Cup Round 2',
-            'description':''
-        },
-        'ticketURL': 'https://www1.ticketmaster.com/us-open-cup-round-2-sacramento-republic-fc-vs-oakland-roots/event/1C00586488FB3406'
-    },
-    {
-        'team':'New Mexico United',
-        'date':'TBD',
-        'isoDate': '2020-04-18',
-        'month':'April',
-        'monthShort':'Apr',
-        'location':'Papa Murphy&apos;s Park',
-        'presaleDescription':'',
-        'broadcast':'KQCA My58, Estrella TV Sacramento, ESPN+',
-        'time':'TBD',
-        'active': true ,
-        'promoContent': {
-            'title':'First Responders Night',
-            'description':''
-        },
-        'ticketURL': 'https://www1.ticketmaster.com/sacramento-republic-fc-vs-new-mexico-united/event/1C00583DBAFB77A0'
-    },
-    {
-        'team':'San Diego Loyal',
-        'date':'Saturday, May 2',
-        'isoDate': '2020-05-02',
-        'month':'May',
-        'monthShort':'May',
-        'location':'Papa Murphy&apos;s Park',
-        'presaleDescription':'',
-        'broadcast':'KQCA My58, Estrella TV Sacramento, ESPN+',
-        'time':'7:30 PM',
-        'active': true ,
-        'promoContent': {
-            'title':'Education Night',
-            'description':''
-        },
-        'ticketURL': 'https://www.ticketmaster.com/sacramento-republic-fc-vs-san-diego-loyal-sc/event/1C00583DBAFF77B1'
-    },
-    {
-        'team':'El Paso Locomotive FC',
-        'date':'Saturday, May 30',
-        'isoDate': '2020-05-30',
-        'month':'May',
-        'monthShort':'May',
-        'location':'Papa Murphy&apos;s Park',
-        'presaleDescription':'',
-        'broadcast':'KQCA My58, Estrella TV Sacramento, ESPN+',
-        'time':'7:30 PM',
-        'active': true ,
-        'promoContent': {
-            'title':'PRIDE',
-            'description':''
-        },
-        'ticketURL': 'https://www.ticketmaster.com/sacramento-republic-fc-vs-el-paso-locomotive/event/1C00583DBB0277C1'
+      'firstName': 'Toni',
+      'lastName': 'Braxton',
+      'fullName': 'Toni Braxton',
+      'role': 'CEO',
+      'vacationHoursRemaining':'19',
+      'healthPlanName':'Health Plan A',
+      'startDate':'2018-01-14',
+      'directManager':''
+
     }]
     //end testJsonObject
-
-    firstName
-    lastName
-    role
-    vacationHoursRemaining
-    healthPlanName
