5ffafd3f19b574e3c7df18c3938202494aea9742
Joshua Forman <cyborgforman@gmail.com>
2020-04-06 12:13:28 -0700

    add quickLinks component and associated scss

--- src/components/EmployeeHome.jsx
+++ src/components/EmployeeHome.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../scss/_employeeHome.scss'
 import Header from './Header'
+import QuickLinks from './QuickLinks'
 
 function EmployeeHome(){
   return(
     <div className='EmployeeHome'>
       <Header />
-
+      <div>
+        <h2>Employee Name Goes Here</h2>
+      </div>
+      <QuickLinks />
     </div>
   )
 }
---
+++ src/components/QuickLinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss/_quickLinks.scss';
+
+function QuickLinks(){ 
+  return(
+    <div className='quickLinksWrapper'>
+      quickLinks go here
+    </div>
+  )
+}
+
+export default QuickLinks;
