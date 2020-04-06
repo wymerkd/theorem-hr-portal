5470c0aefd15fb6abc15262fe4e8a8eed3435d00
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-06 11:00:01 -0700

    lay out basic structure for FeedbackSubmit form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       masterEmployeeList: [],
-      selectedEmployee: {}
+      selectedEmployee: {},
+      employeeFeedback: []
     };
   }
 
--- src/components/FeedbackSubmit.jsx
+++ src/components/FeedbackSubmit.jsx
@@ -1,2 +1,49 @@
-import '../scss/_feedbackSubmit.scss'
 //this is going to be a portal where the user can submit anonymous feedback.
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+import '../scss/_feedbackSubmit.scss'
+
+
+export default function FeedbackSubmit(props){
+
+  let _subject = null;
+  let _content = null;
+
+function handleNewFeedbackSubmission(event) {
+  event.preventDefault();
+  props.onFeedbackCreation({subject: _subject.value, date: new Date(), content: _content.value, feedbackid: v4()});
+  _subject.value = '';
+  _content.value = '';
+}
+
+  return(
+    <div>
+          <div>
+            <h2>Add Feedback </h2>
+
+            <form onSubmit={handleNewPostFormSubmission}>
+              <input style={input}
+                type='text'
+                id='subject'
+                placeholder='Feedback Subject'
+                ref={(input) => {_subject = input;}}/>
+              <textarea
+                id='content'
+                type='text'
+                placeholder="Write feedback here"
+                ref={(input) => {_content = input;}}/>
+
+              <div>  <button type='submit'>Submit Feedback</button> </div>
+            </form>
+
+          </div>
+        </div>
+  );
+}
+
+NewPostForm.propTypes = {
+  onPostCreation: PropTypes.func
+};
+
+//need to import a function that sends feedback to email address, and to kyle's admin view component
