3de2e91efd021974c0b3017e903d56d047967689
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-06 15:06:44 -0700

    give adminview access to state vars

--- src/components/AdminView.jsx
+++ src/components/AdminView.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import '../scss/_adminView.scss'
+import PropTypes from 'prop-types';
+
 
 //comment about functionality
 
-const feedbackList = {
-  feedback: [{subject: 'No more bog roll in the loo!'}],
-  displayFeedback: function () {
-    if (this.feedback.length === 0) {
-      console.log('There is currently no employee feedback.')
-    } else {
-      console.log('Employee Feedback:');
-      for (let i = 0; i < this.feedback.length; i++) {
-        console.log(this.feedback[i].subject);
-      }
-    }
-    this.displayFeedback();
-  }
-};
 
-const AdminView = () => {
+export default function AdminView(props){
+
+// const feedbackList = {
+//   feedback: [{subject: 'No more bog roll in the loo!'}],
+//   displayFeedback: function () {
+//     if (this.feedback.length === 0) {
+
+//       console.log('There is currently no employee feedback.')
+//     } else {
+//       console.log('Employee Feedback:');
+//       for (let i = 0; i < this.feedback.length; i++) {
+//         console.log(this.feedback[i].subject);
+//       }
+//     }
+//     this.displayFeedback();
+//   }
+// };
+
+
   return (
-    <p>heyo</p>
+    <p>{props.masterEmployeeFeedbackList[0].content}</p>
   );
+
+
 }
 
-export default AdminView;
+
+AdminView.propTypes = {
+  masterEmployeeFeedbackList: PropTypes.array
+};
