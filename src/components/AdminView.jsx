a3b775dfbfcc3a6210633b156ea9bf4c14736e4a
wymerkd <wymerkd@gmail.com>
2020-04-07 14:07:08 -0700

    add conditional rendering, short circuit evaluation to display div, and map to display all feedback.

--- src/components/AdminView.jsx
+++ src/components/AdminView.jsx
@@ -3,35 +3,29 @@ import '../scss/_adminView.scss'
 import PropTypes from 'prop-types';
 
 
-//comment about functionality
+export default function AdminView(props) {
+  const feedback = props.masterEmployeeFeedbackList;
 
-
-export default function AdminView(props){
-
-// const feedbackList = {
-//   feedback: [{subject: 'No more bog roll in the loo!'}],
-//   displayFeedback: function () {
-//     if (this.feedback.length === 0) {
-
-//       console.log('There is currently no employee feedback.')
-//     } else {
-//       console.log('Employee Feedback:');
-//       for (let i = 0; i < this.feedback.length; i++) {
-//         console.log(this.feedback[i].subject);
-//       }
-//     }
-//     this.displayFeedback();
-//   }
-// };
-
+  // Checks if feedback object is empty or not
+  const isFeedbackPresent = (feedback.length > 0) ? true : false;
 
   return (
-    <p>{props.masterEmployeeFeedbackList[0].content}</p>
+    // Short circuit evaluation displays corresponding div depending on result of feedback condition
+    <div>
+      {isFeedbackPresent && <div>
+        {feedback.map((key, index) => {
+          return <div key={index}>
+            <p>Subject: {key.subject}</p>
+            <p>Date: {key.date.toString()}</p>
+            <p>Content: {key.content}</p></div>;
+        })}
+      </div>}
+      {!isFeedbackPresent && <div>
+        <p>There is currently no employee feedback.</p>
+      </div>}
+    </div>
   );
-
-
 }
-
 
 AdminView.propTypes = {
   masterEmployeeFeedbackList: PropTypes.array
