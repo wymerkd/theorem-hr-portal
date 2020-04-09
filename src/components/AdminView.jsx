27fa2980d24c86eea9c3b1a294e8af682f0ccf22
wymerkd <wymerkd@gmail.com>
2020-04-09 14:17:37 -0700

    add container-flex to feedback inbox, add padding

--- src/components/AdminView.jsx
+++ src/components/AdminView.jsx
@@ -16,7 +16,8 @@ export default function AdminView(props) {
 
   return (
     // Short circuit evaluation displays corresponding div depending on result of feedback condition
-    <div className="container inbox">
+    <div className= "containFeedback">
+      <div className="container-flex inbox">
       {/* Displays if feedback is present */}
         <div className="row feedback">
           <div className="col-sm">Subject</div>
@@ -28,32 +29,33 @@ export default function AdminView(props) {
         {feedback.map((item, index) => {
 
           return <div>
-            <div className="row feedbackBody" key={index}>
-              <div className="col-sm">{item.subject}</div>
-              <div className="col-sm">{item.date.toString().slice(0, 21)}</div>
-              <div className="col-sm"><button className="toggleButton" id={`item${item.feedbackid}`}>View Content</button></div>
-              <div className="col-sm"><button className="deleteButton" onClick={() => {if (window.confirm('Are you sure you wish to delete this item?')) props.onHandleDelete(item.feedbackid)}} >Delete</button></div>
-            </div>
-            <div className="row dropdown">
-              <div className="col-xl-12 noPadding">
-                <UncontrolledCollapse toggler={`#item${item.feedbackid}`}>
-                  <Card>
-                    <CardBody>
-                    <p>{item.content}</p>
-                    </CardBody>
-                  </Card>
-                </UncontrolledCollapse>
+              <div className="row feedbackBody" key={index}>
+                <div className="col-sm">{item.subject}</div>
+                <div className="col-sm">{item.date.toString().slice(0, 21)}</div>
+                <div className="col-sm"><button className="toggleButton" id={`item${item.feedbackid}`}>View Content</button></div>
+                <div className="col-sm"><button className="deleteButton" onClick={() => {if (window.confirm('Are you sure you wish to delete this item?')) props.onHandleDelete(item.feedbackid)}} >Delete</button></div>
+              </div>
+              <div className="row dropdown">
+                <div className="col-xl-12 noPadding">
+                  <UncontrolledCollapse toggler={`#item${item.feedbackid}`}>
+                    <Card>
+                      <CardBody>
+                      <p>{item.content}</p>
+                      </CardBody>
+                    </Card>
+                  </UncontrolledCollapse>
+                </div>
               </div>
             </div>
+          })}
+        </div>}
+        {/* Displays if there is no feedback */}
+        {!isFeedbackPresent && <div>
+          <div className="row feedbackBody">
+            <div className="col-xl">There is currently no employee feedback.</div>
           </div>
-        })}
-      </div>}
-      {/* Displays if there is no feedback */}
-      {!isFeedbackPresent && <div>
-        <div className="row feedbackBody">
-          <div className="col-xl">There is currently no employee feedback.</div>
-        </div>
-      </div>}
+        </div>}
+      </div>
     </div>
   );
 }
